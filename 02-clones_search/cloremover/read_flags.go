750795e735a6c871278baaab036204222bfd6301
Serg Kotovsky <info@geekserg.ru>
2021-04-04 22:30:33 +0300

    добавлены флаги для уровня логирования и имени лог-файла

--- 02-clones_search/cloremover/read_flags.go
+++ 02-clones_search/cloremover/read_flags.go
@@ -11,15 +11,19 @@ type ConfigType struct {
 	ConfirmFlag string
 	ShowFiles   uint8
 	DirLimit    uint8
+	LogFile     string
+	LogLevel    string
 }
 
 func ReadFlags(conf *ConfigType) error {
 
 	var (
-		removeFlag  = flag.Bool("remove", false, "set the flag if you wish to delete some clone files")
+		removeFlag  = flag.Bool("remove", false, "set this flag if you wish to delete some clone files")
 		confirmFlag = flag.String("confirm", "on", "confirmation before removing files")
 		showFlag    = flag.Int("files", 10, "specifies the amount of different clones to be displayed")
 		limitFlag   = flag.Int("dirs", 0, "determines maximum number of directories to be displayed, default is 'no limit'")
+		logFile     = flag.String("log", "cloremover.log", "the name for log-file")
+		logLevel    = flag.String("loglevel", "info", "determines what kind of events should be logged")
 	)
 
 	flag.Parse()
@@ -39,12 +43,18 @@ func ReadFlags(conf *ConfigType) error {
 	if *limitFlag < 0 || *limitFlag > 255 {
 		return fmt.Errorf("Incorrect 'limit' flag value. Expected value >=0, got %d", *limitFlag)
 	}
+	// data validation for loglevel
+	if *logLevel != "info" && *logLevel != "error" && *logLevel != "debug" {
+		return fmt.Errorf("Incorrect 'loglevel' flag value. Expected debug/info/error, got %s", *logLevel)
+	}
 
 	conf.DirPath = flag.Arg(0)
 	conf.RemoveFlag = *removeFlag
 	conf.ConfirmFlag = *confirmFlag
 	conf.ShowFiles = uint8(*showFlag)
 	conf.DirLimit = uint8(*limitFlag)
+	conf.LogFile = *logFile
+	conf.LogLevel = *logLevel
 
 	return nil
 
