9db44937a5bfe632911b7c0c23454289d78aa0d1
Serg Kotovsky <info@geekserg.ru>
2021-04-04 22:31:15 +0300

    добавлены вызовы логгера

--- 02-clones_search/cloremover/remove_clones.go
+++ 02-clones_search/cloremover/remove_clones.go
@@ -5,19 +5,24 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"github.com/sirupsen/logrus"
 )
 
-func Remove(fileSlice []fileData, conf *ConfigType, outputMap map[uint32]uint32) {
+func Remove(fileSlice []fileData, conf *ConfigType, outputMap map[uint32]uint32, log logrus.FieldLogger) {
 	// removeFlag was not set
 	if !conf.RemoveFlag {
+		log.Debug("The flag '-remove' was not set.")
 		return
 	}
 
 	userChoice, err := chooseFile(uint32(len(outputMap)))
 	if err != nil {
+		log.Error(err)
 		fmt.Println(err)
 		return
 	}
+	log.Debugf("user choice about a file to delete is %d", userChoice)
 
 	// print data about chosen file
 	onceFlag := false
@@ -26,8 +31,8 @@ func Remove(fileSlice []fileData, conf *ConfigType, outputMap map[uint32]uint32)
 	for i, fileData := range fileSlice {
 		if fileData.id == outputMap[uint32(userChoice)] {
 			if !onceFlag {
-				fmt.Println()
-				fmt.Printf("Chosen file: %s - %d bytes:\n", fileData.fileName, fileData.sizeInBytes)
+				log.Debug("Chosen file: %s - %d bytes", fileData.fileName, fileData.sizeInBytes)
+				fmt.Printf("\nChosen file: %s - %d bytes:\n", fileData.fileName, fileData.sizeInBytes)
 				onceFlag = true
 			}
 
@@ -39,24 +44,30 @@ func Remove(fileSlice []fileData, conf *ConfigType, outputMap map[uint32]uint32)
 
 	userChoice, err = chooseDir(uint32(len(dirMap)))
 	if err != nil {
+		log.Error(err)
 		fmt.Println(err)
 		return
 	}
+	log.Debugf("user choice about a directory to delete a file from is %d", userChoice)
 
 	fileToDelete := fileSlice[dirMap[userChoice]]
 	// delete the file !!!!!!!!!!
 	if conf.ConfirmFlag == "on" {
-		confirmation := confirmRemove(fileToDelete)
+		confirmation := confirmRemove(fileToDelete, log)
 		if !confirmation {
+			log.Info("user refused file removal")
 			fmt.Println("file was not deleted")
 			return
 		}
 	}
 
 	filePath := filepath.Join(fileToDelete.dir, fileToDelete.fileName)
+	log.Debugf("file to be deleted is %s", filePath)
 	if err := os.Remove(filePath); err != nil {
+		log.Errorf("error while file removal, %v", err)
 		fmt.Println(err)
 	}
+	log.Infof("file %s was successfully deleted", filePath)
 	fmt.Printf("file %s was successfully deleted", filePath)
 }
 
@@ -113,23 +124,26 @@ func chooseDir(numOfDirs uint32) (uint32, error) {
 }
 
 // user dialog to confirm dile removing
-func confirmRemove(fileToDelete fileData) bool {
+func confirmRemove(fileToDelete fileData, log logrus.FieldLogger) bool {
 	fmt.Printf("you are about to delete the file %s from directory %s, %d bytes in size", fileToDelete.fileName, fileToDelete.dir, fileToDelete.sizeInBytes)
 	var userInput string
 	// choose and remove a file
 	fmt.Print("Please, enter 'yes' to confirm file removing. For exit enter 'q': ")
 	_, err := fmt.Scanln(&userInput)
 	if err != nil {
+		log.Errorf("error while user input about file removal confirmation, %v", err)
 		fmt.Println(err)
 		return false
 	}
 	// user want to quit
 	if userInput == "q" || userInput == "Q" {
+		log.Debug("Entered 'q', no files to be deleted. Program exit.")
 		fmt.Println("Entered 'q', no files to be deleted. Program exit.")
 		return false
 	}
 	// user want to delete a file frome specified directory
 	if userInput == "yes" {
+		log.Debug("Entered 'yes', file will be deleted.")
 		return true
 	}
 	return false
